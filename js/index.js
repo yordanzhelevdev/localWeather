c8a40ffef0cfcc2937cebef23174435244636958
Yordan Zhelev <yordanzhelev950711@gmail.com>
2018-05-11 14:38:30 +0300

    Reconstructured js code

--- js/index.js
+++ js/index.js
@@ -1,103 +1,75 @@
-function getLocation() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(myPosition);
-  } else {
-    alert("Geolocation is not supported by this browser.");
+window.onload = () => {
+  (function getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(myPosition);
+    } else {
+      alert("Geolocation is not supported by this browser.");
+    }
+  }())
+
+  function myPosition(pos) {
+
+    //request to the server
+    const xhttp= new XMLHttpRequest(),
+          {latitude, longitude} = pos.coords,
+          url = `https://api.apixu.com/v1/current.json?key=9e4f604fae6149328e8175716170906&q=${latitude},${longitude}`;
+
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        //tuk parsvame datata v json format
+        const myObj = JSON.parse(this.responseText);
+        const infoDiv = document.getElementById("weather-div"); // Info Div
+
+        //location name and country
+        const loc = document.createElement("p");
+        loc.id = "locName";
+        const locT = document.createTextNode(myObj.location.name + "/" + myObj.location.country);
+        loc.appendChild(locT);
+        document.getElementById("loc-div").appendChild(loc);
+
+        //Weather status - text
+        const weatherStatusT = document.createElement("p");
+        weatherStatusT.id = "weatherText";
+        const wText = document.createTextNode(myObj.current.condition.text);
+        weatherStatusT.appendChild(wText);
+        infoDiv.appendChild(weatherStatusT);
+
+
+        //Weather status - icon
+        const weatherStatus = document.createElement("img");
+        weatherStatus.src = "http://" + myObj.current.condition.icon;
+        weatherStatus.id = "weatherStatus";
+        infoDiv.appendChild(weatherStatus);
+
+        //Temp Celsius
+        const tempC = document.createElement("p");
+        const tempCText = document.createTextNode(myObj.current.temp_c + '\u2103');
+        tempC.appendChild(tempCText);
+        infoDiv.appendChild(tempC);
+
+
+
+        //Celsius to Fahr button
+        const btn = document.createElement("button");
+        btn.id = "btnCel";
+        const btnT = document.createTextNode("Fahrenheit");
+        btn.appendChild(btnT);
+
+
+        function convertDegrees() {
+          if (btn.innerHTML === "Fahrenheit") {
+            tempC.innerHTML = myObj.current.temp_f + "\u2109";
+            btn.innerHTML = "Celsius";
+          } else if (btn.innerHTML === "Celsius") {
+            tempC.innerHTML = myObj.current.temp_c + "\u2103";
+            btn.innerHTML = "Fahrenheit";
+          }
+        }
+        btn.addEventListener('click', convertDegrees)
+        infoDiv.appendChild(btn);
+      }
+    };
+    xhttp.open("GET", url, true);
+    xhttp.send();
   }
 }
-
-function myPosition(pos) {
-  var latitude = pos.coords.latitude;
-  var longitude = pos.coords.longitude;
-  
-  //loging location
-  console.log(latitude);
-  console.log(longitude);
-
-  //request to the server
-  var xhttp = new XMLHttpRequest();
-  var url =
-    "https://api.apixu.com/v1/current.json?key=9e4f604fae6149328e8175716170906&q=" +
-    latitude +
-    "," +
-    longitude +
-    "";
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      //tuk parsvame datata v json format
-      myObj = JSON.parse(this.responseText);
-      
-      
-      var infoDiv = document.getElementById("weather-div");// Info Div
-       
-      
-      //Weather status//
-      
-      
-       //location name and country
-      var loc = document.createElement("p");
-      loc.id = "locName";
-      var locT = document.createTextNode(myObj.location.name + "/" + myObj.location.country);
-      loc.appendChild(locT);
-      document.getElementById("loc-div").appendChild(loc);
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      //Weather status - text
-      var weatherStatusT = document.createElement("p");
-      weatherStatusT.id = "weatherText";
-      var wText = document.createTextNode(myObj.current.condition.text);
-      weatherStatusT.appendChild(wText);
-      infoDiv.appendChild(weatherStatusT);
-      
-      
-      //Weather status - icon
-      var weatherStatus = document.createElement("img");
-      weatherStatus.src = "http://"+myObj.current.condition.icon;
-      weatherStatus.id = "weatherStatus";
-      infoDiv.appendChild(weatherStatus);
-     
-      //Temp Celsius
-      var tempC = document.createElement("p");
-      var tempCText = document.createTextNode(myObj.current.temp_c + '\u2103');
-      tempC.appendChild(tempCText);
-      infoDiv.appendChild(tempC);
-      
-     
-      
-      //Celsius to Fahr button
-      var btn = document.createElement("button");
-      btn.id = "btnCel";
-      var btnT = document.createTextNode("Fahrenheit");
-      btn.appendChild(btnT);
-      
-      btn.onclick = function(){
-        if(btn.innerHTML === "Fahrenheit"){
-          tempC.innerHTML = myObj.current.temp_f + "\u2109";
-          btn.innerHTML = "Celsius";
-        }
-        else if(btn.innerHTML === "Celsius"){
-          tempC.innerHTML = myObj.current.temp_c + "\u2103";
-          btn.innerHTML = "Fahrenheit";
-        }
-      };
-      infoDiv.appendChild(btn);
-      
-      
-    }    
-  };
-  xhttp.open("GET", url, true);
-  xhttp.send();
-
-  //console.log(url);
-}
-
-//there we call the function to get the user position
-getLocation();
